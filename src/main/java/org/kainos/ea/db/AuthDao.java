60530106fd853384f8cb0179087b8edc064f9d2e
BlankAmber <145449058+BlankAmber@users.noreply.github.com>
2023-12-06 18:34:01 +0000

    Add login backend

---
+++ src/main/java/org/kainos/ea/db/AuthDao.java
@@ -0,0 +1,68 @@
+package org.kainos.ea.db;
+
+import org.apache.commons.lang3.time.DateUtils;
+import org.kainos.ea.Util;
+import org.kainos.ea.cli.Login;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.UUID;
+
+public class AuthDao {
+    private static final int TOKEN_EXPIRY_NUM_HOURS_AFTER_LOGIN = 8;
+
+    public boolean isValidLogin(Login login) {
+        try (Connection conn = DatabaseConnector.getConnection()) {
+            Statement statement = conn.createStatement();
+
+            ResultSet resultSet = statement.executeQuery(
+                    "SELECT salt, secured_password FROM user WHERE username = "
+                    + "'" + login.getUsername() + "'");
+
+            if (resultSet.next()) {
+                String salt = resultSet.getString("salt");
+                String saltedPassword = login.getPassword() + salt;
+
+                MessageDigest messageDigest = null;
+                try {
+                    messageDigest = MessageDigest.getInstance("SHA-256");
+                } catch (NoSuchAlgorithmException e) {
+                    System.err.println("Algorithm not found!");
+                }
+
+                byte[] encodedHash = messageDigest.digest(saltedPassword.getBytes());
+                String securedPassword = String.format("%064x", new BigInteger(1, encodedHash));
+
+                return resultSet.getString("secured_password").equals(securedPassword);
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+
+        return false;
+    }
+
+    public String generateToken(String username) throws SQLException {
+        String token = UUID.randomUUID().toString();
+        // TODO: Test this with different timezones
+        Date expiryDate = DateUtils.addHours(new Date(), TOKEN_EXPIRY_NUM_HOURS_AFTER_LOGIN);
+
+        Connection c = DatabaseConnector.getConnection();
+
+        String insertStatement = "INSERT INTO token (username, token, expiry_date) VALUES (?,?,?)";
+
+        PreparedStatement statement = c.prepareStatement(insertStatement);
+        Util.setValues(statement, username, token, new Timestamp(expiryDate.getTime()));
+        statement.executeUpdate();
+
+        return token;
+    }
+}
