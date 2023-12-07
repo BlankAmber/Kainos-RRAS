1aa9fd3b2f1f8633b32fa819aa1759f5ec929b63
BlankAmber <145449058+BlankAmber@users.noreply.github.com>
2023-12-07 10:14:16 +0000

    WIP Job roles

---
+++ src/main/java/org/kainos/ea/db/JobRolesDao.java
@@ -0,0 +1,33 @@
+package org.kainos.ea.db;
+
+import org.kainos.ea.cli.JobRole;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JobRolesDao {
+    public List<JobRole> getAllJobRoles() throws SQLException {
+        Connection connection = DatabaseConnector.getConnection();
+
+        Statement s = connection.createStatement();
+
+        ResultSet rs = s.executeQuery("SELECT job_role_id, job_role_name FROM job_roles");
+
+        List<JobRole> jobRolesList = new ArrayList<>();
+
+        while (rs.next()) {
+            JobRole jobRoles = new JobRole(
+                    rs.getInt("job_roles_id"),
+                    rs.getString("job_roles_name")
+
+            );
+
+            jobRolesList.add(jobRoles);
+        }
+        return jobRolesList;
+    }
+}
