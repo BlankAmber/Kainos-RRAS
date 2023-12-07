82a0cdb5af3a0e2b737b9d401de9bfcf45a75b1c
Ryan McAlary <ryan.mcalary@kainos.com>
2023-12-07 10:30:40 +0000

    View Job Roles Working

--- src/main/java/org/kainos/ea/DropwizardWebServiceApplication.java
+++ src/main/java/org/kainos/ea/DropwizardWebServiceApplication.java
@@ -6,6 +6,7 @@ import io.dropwizard.setup.Environment;
 import io.federecio.dropwizard.swagger.SwaggerBundle;
 import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
 import org.kainos.ea.resources.AuthController;
+import org.kainos.ea.resources.JobRolesController;
 
 public class DropwizardWebServiceApplication
         extends Application<DropwizardWebServiceConfiguration> {
@@ -35,5 +36,6 @@ public class DropwizardWebServiceApplication
     public void run(final DropwizardWebServiceConfiguration configuration,
                     final Environment environment) {
         environment.jersey().register(new AuthController());
+        environment.jersey().register(new JobRolesController());
     }
 }
