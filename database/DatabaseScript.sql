02a7f28f1a699d1aad1412e1ece8ec10a2f9a9c7
Ryan McAlary <ryan.mcalary@kainos.com>
2023-12-20 10:54:52 +0000

    Database Update

--- database/DatabaseScript.sql
+++ database/DatabaseScript.sql
@@ -26,7 +26,41 @@ CREATE TABLE token (
     expiry_date DATETIME NOT NULL
 );
 
--- update job_roles table
-ALTER TABLE job_roles
+CREATE TABLE job_role(
+	job_role_id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    job_role_name varchar(64) NOT NULL
+);
+
+-- update job_role table
+ALTER TABLE job_role
 ADD COLUMN specification_summary VARCHAR(4000),
 ADD COLUMN sharepoint_link VARCHAR(1000);
+
+
+
+ALTER TABLE job_role
+ADD COLUMN job_family_id int,
+ADD CONSTRAINT fk_job_role_job_family
+FOREIGN KEY (job_family_id) REFERENCES job_family(job_family_id);
+
+
+
+-- creation of job_family_group table
+CREATE TABLE job_family_group (
+    job_family_group_id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    job_family_group_name varchar(64) NOT NULL
+);
+
+-- creation of job_family table
+CREATE TABLE job_family (
+    job_family_id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    job_family_name varchar(64) NOT NULL,
+    job_family_group_id int,
+    FOREIGN KEY (job_family_group_id) REFERENCES job_family_group(job_family_group_id)
+);
+
+SELECT j.job_role_id, j.job_role_name, jfg.job_family_group_name
+FROM job_role j
+JOIN job_family jf ON j.job_family_id = jf.job_family_id
+JOIN job_family_group jfg ON jf.job_family_group_id = jfg.job_family_group_id;
+
