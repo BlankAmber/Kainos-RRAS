3a3235a2c2d3758b26b97fbffb2523344e8cb955
BlankAmber <145449058+BlankAmber@users.noreply.github.com>
2023-12-30 21:38:43 +0000

    Add formality dataset and split-dataset.py

---
+++ ai/README.md
@@ -0,0 +1,9 @@
+# AI
+
+Credit
+---
+- `corpus/formality-dataset-dict.pkl` is a slightly modified version of `preprocessing/corpus_dict.pkl` from [YonglinWang-Brandeis](https://github.com/YonglinWang-Brandeis/text-formality-classifier)
+
+How to run
+---
+1. Run `corpus/split-dataset.py` to generate datasets for training, validation, and testing
---
+++ ai/corpus/split-dataset.py
@@ -0,0 +1,24 @@
+# Credit to: https://github.com/YonglinWang-Brandeis/text-formality-classifier
+# formality-dataset-dict.pkl is a slightly modified version of their preprocessing/corpus_dict.pkl
+
+from math import floor
+import pickle
+
+# Training : Validation : Testing
+SPLIT_PERCENT = (70, 15, 15)
+assert(sum(SPLIT_PERCENT) == 100)
+
+with open("ai/corpus/formality-dataset-dict.pkl", "rb") as dataset_file:
+    dataset_dict = pickle.load(dataset_file)
+
+num_sentences = len(dataset_dict["formal"])
+start_index = 0
+for i, label in enumerate(("training", "validation", "testing")):
+    percent = SPLIT_PERCENT[i]
+    end_index = start_index + floor(percent / 100 * num_sentences)
+    if i == 2:
+        end_index += 1
+    split_dataset_dict = {k: v[start_index:end_index] for k, v in dataset_dict.items()}
+    start_index = end_index
+    with open(f"ai/corpus/formality-dataset-dict-{label}.pkl", "wb") as split_dataset_file:
+        pickle.dump(split_dataset_dict, split_dataset_file)
