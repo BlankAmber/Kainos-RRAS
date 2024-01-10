7e70b5ee48ceb6e8df9fa19af62cd0ffa285f4ac
Adam Marshall <adam.marshall@kainos>
2024-01-10 09:29:02 +0000

    Fix spacing

--- src/main/java/org/kainos/ea/db/JobRolesDao.java
+++ src/main/java/org/kainos/ea/db/JobRolesDao.java
@@ -38,13 +38,13 @@ public class JobRolesDao {
     public JobRole getJobRoleById(Connection conn, int id) throws SQLException {
         Statement statement = conn.createStatement();
         ResultSet resultSet = statement.executeQuery(
-                 "SELECT j.job_role_id, j.job_role_name, jfg.job_family_group_name, "
-                    + "ml.management_level_name, j.sharepoint_link, j.specification_summary, j.responsibilities "
-                    + "FROM job_role j "
-                    + "JOIN job_family jf ON j.job_family_id = jf.job_family_id "
-                    + "JOIN job_family_group jfg ON jf.job_family_group_id = jfg.job_family_group_id "
-                    + "JOIN management_level ml ON j.management_level_id = ml.management_level_id "
-                    + "where j.job_role_id = " + id);
+                "SELECT j.job_role_id, j.job_role_name, jfg.job_family_group_name, "
+                + "ml.management_level_name, j.sharepoint_link, j.specification_summary, j.responsibilities "
+                + "FROM job_role j "
+                + "JOIN job_family jf ON j.job_family_id = jf.job_family_id "
+                + "JOIN job_family_group jfg ON jf.job_family_group_id = jfg.job_family_group_id "
+                + "JOIN management_level ml ON j.management_level_id = ml.management_level_id "
+                + "where j.job_role_id = " + id);
 
         if (resultSet.next()) {
             return new JobRole(
